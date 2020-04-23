a958878e863e666e2b27997c12432c5cc3083284
v.shapovalov <nobody.one05111999@gmail.com>
2020-04-23 18:16:37 +0700

    styles

---
+++ Web/Controllers/CalcServiceController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class CalcServiceController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CalcServiceController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult PassUsingModel()
+        {
+            ValueModel viewModel = new ValueModel();
+            return View(viewModel);
+        }
+        public IActionResult PassUsingViewData()
+        {
+            ViewData["Value"] = new ValueModel();
+
+            return View();
+        }
+
+        public IActionResult PassUsingViewBag()
+        {
+            ViewBag.Value = new ValueModel();
+            return View();
+        }
+
+        public IActionResult AccessServiceDirectly()
+        {
+            ValueModel viewModel = new ValueModel();
+            return View(viewModel);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web/Models/ValueModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class ValueModel
+    {
+        public readonly int randValueFirst, randValueSecond;
+        public ValueModel()
+        {
+            Random rnd = new Random();
+            randValueFirst = rnd.Next(-10, 10);
+            randValueSecond = rnd.Next(-10, 10);
+        }
+    }
+}
