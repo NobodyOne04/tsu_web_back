c7ea04815a27049e6449198057a52bdf87e1bb72
v.shapovalov <nobody.one05111999@gmail.com>
2020-05-14 14:04:19 +0700

    lab3

---
+++ Web/Controller/MockupsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class MockupsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Quiz()
+        {
+            Operations operations = new Operations();
+            return View(operations);
+        }
+
+        [HttpPost]
+        public IActionResult Quiz(Operations operation, string action)
+        {
+            double number;
+            if (double.TryParse(operation.YourAnswer, out number) & ModelState.IsValid)
+            {
+                CorrectAnswers answers = CorrectAnswers.Instance;
+                answers.Total+= 1;
+                operation.Solution(); 
+                ViewData["NotANumber"] = "" + operation.First + 
+                    "  ||  " + operation.Second;
+                if (operation.Check())
+                    answers.Correct += 1;
+                (answers.Answers).Add(operation);
+            }
+            else
+                ViewData["NotANumber"] = "Not a number!";
+            if (action == "Next")
+                return View(new Operations());
+            return RedirectToAction("QuizResult");
+        }
+
+        public IActionResult QuizResult()
+        {
+            CorrectAnswers answers = CorrectAnswers.Instance;
+            ViewBag.Result = answers.Answers;
+            ViewData["Correct"] = "" + answers.Correct;
+            ViewData["Total"] = "" + answers.Total;
+            return View();
+        }
+    }
+}
---
+++ Web/Models/OperationsModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class Operations
+    {
+        [Required]
+        [StringLength(5, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 1)]
+        public string First { get; set; }
+        [Required]
+        [StringLength(5, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 1)]
+        public string Second { get; set; }
+        public string Operand { get; set; }
+        public string CorrectAnswer { get; private set; }
+        public string YourAnswer { get; set; }
+
+        public Operations()
+        {
+            Random random = new Random();
+            double First = random.Next(11);
+            double Second = random.Next(11);
+            string[] Operands = { "+", "-", "*", "/" };
+            this.Operand = Operands[random.Next(4)];
+            this.First = "" + First;
+            this.Second = "" + Second;
+        }
+        public void Solution()
+        {
+            StringToOperands(Operand, Convert.ToDouble(this.First), Convert.ToDouble(this.Second));
+        }
+
+        private void  StringToOperands(string oper, double First, double Second)
+        {
+            switch (oper)
+            {
+                case "+":
+                    CorrectAnswer = "" + (First + Second);
+                    break;
+                case "-":
+                    CorrectAnswer = "" + (First - Second);
+                    break;
+                case "*":
+                    CorrectAnswer = "" + (First * Second);
+                    break;
+                case "/":
+                    CorrectAnswer = "" + (First / Second);
+                    break;
+            }
+        }
+        public bool Check()
+        {
+            double AnswerDouble = Math.Abs(Convert.ToDouble(YourAnswer) - Convert.ToDouble(CorrectAnswer));
+            if (Operand == "/" & AnswerDouble < 0.1)
+            {
+                return true;
+            }
+            if (YourAnswer == CorrectAnswer)
+                return true;
+            return false;
+        }
+    }
+
+    public class CorrectAnswers
+    {
+        private CorrectAnswers()
+        {
+            Number = 0;
+            Correct = 0;
+            Total = 0;
+            Answers = new List<Operations>();
+        }
+        
+        public List<Operations> Answers;
+        public int Correct { get; set; }
+        public int Total { get; set; }
+        public int Number { get; set; }
+
+        public static CorrectAnswers Instance { get; } = new CorrectAnswers();
+
+    }
+}
