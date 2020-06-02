3ac589dffe6650db6c188d67e9478822cec21913
v.shapovalov <nobody.one05111999@gmail.com>
2020-06-02 23:11:29 +0700

    init

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -3,38 +3,111 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Web.Data;
+using Web.Models;
+using Web.Services;
+using Microsoft.AspNetCore.Identity;
 
 namespace Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<IUserPermissionsService, UserPermissionsService>();
+            services.AddScoped<MimeMappingService>();
+
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            using (var context = scope.ServiceProvider.GetService<ApplicationDbContext>())
+            {
+                context.Database.Migrate();
+                this.ConfigureIdentity(scope).GetAwaiter().GetResult();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            else
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "forum",
+                    template: "Forum",
+                    defaults: new { controller = "ForumCategories", action = "Index" });
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Posts}/{action=Index}/{id?}");
             });
+        }
+
+
+
+        private async Task ConfigureIdentity(IServiceScope scope)
+        {
+            var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+            var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+
+            var adminsRole = await roleManager.FindByNameAsync(ApplicationRoles.Administrators);
+            if (adminsRole == null)
+            {
+                var roleResult = await roleManager.CreateAsync(new IdentityRole(ApplicationRoles.Administrators));
+                if (!roleResult.Succeeded)
+                {
+                    throw new InvalidOperationException($"Unable to create {ApplicationRoles.Administrators} role.");
+                }
+
+                adminsRole = await roleManager.FindByNameAsync(ApplicationRoles.Administrators);
+            }
+
+            var adminUser = await userManager.FindByNameAsync("admin@localhost.local");
+            if (adminUser == null)
+            {
+                var userResult = await userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admin@localhost.local",
+                    Email = "admin@localhost.local"
+                }, "PasswordLab6");
+                if (!userResult.Succeeded)
+                {
+                    throw new InvalidOperationException($"Unable to create admin@localhost.local user");
+                }
+
+                adminUser = await userManager.FindByNameAsync("admin@localhost.local");
+            }
+
+            if (!await userManager.IsInRoleAsync(adminUser, adminsRole.Name))
+            {
+                await userManager.AddToRoleAsync(adminUser, adminsRole.Name);
+            }
         }
     }
 }
